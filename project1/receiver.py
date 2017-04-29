79fbe652371b31fe56f66ed8c474a9bb9a2f2dc2
Severin Davis <severin.davis@gmail.com>
2017-04-29 18:26:19 +0200

    reworked sender and receiver
    needs lab testing. I can't do more until we run it on real hardware.

--- project1/receiver.py
+++ project1/receiver.py
@@ -1,10 +1,46 @@
 #!/usr/bin/python
-import RPi.GPIO as GPIO
+#import RPi.GPIO as GPIO
 import time
+time_slice = 0.5
+bit_time = 0.031
+
 # Use BCM numbering
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(17, GPIO.IN)
-while True:
-    value = GPIO.input(17) 
-    print value 
-    time.sleep(1)
+#GPIO.setmode(GPIO.BCM)
+#GPIO.setup(17, GPIO.IN)
+
+def sample():
+	#read GPIO
+	bit = 0
+	i = 0
+	while i < 7 :
+		#value = GPIO.input(17) 
+		#print value 
+		print(i,  " sample: ", ((time.time()*1000)%1000)/1000.0,)
+		i = i + 1
+		time.sleep(bit_time)
+	return bit
+
+
+def main() :
+	senderlock = 1
+	while True:
+		current_time  = ((time.time()*1000)%1000)/1000.0
+		if (0*0.5) <= current_time and current_time < (0*0.5 + 0.5) :
+			if 0*0.5+0.5 - current_time > 8 * bit_time : 
+				if senderlock == 1: #identifies sender1 entry and forces additional half bit_time wait.
+					senderlock = 0
+					time.sleep(bit_time/2.0) #forces sample in the middle of bit_time
+				print("sender1")	
+				print(sample())
+
+		if (1*0.5) <= current_time and current_time < (1*0.5 + 0.5) :
+			if 1*0.5+0.5 - current_time > 8 * bit_time : 	
+				if senderlock == 0:
+					senderlock = 1
+					time.sleep(bit_time/2.0)
+				print("sender2")
+				print(sample())
+
+
+main()
+
--- project1/sender1.py
+++ project1/sender1.py
@@ -1,60 +1,41 @@
-import RPi.GPIO as GPIO
 import time
+#import RPi.GPIO as GPIO
  
 message = "HELLO WORLD FROM SENDER 1"
 
 sender_id = 0
 time_slice = 0.5
-bit_time = 0.01
+bit_time = 0.005
 
-def send_bit(b):
-	#print(b),
-	time.sleep(bit_time)
+def send_bit(b):																		
+	print(b, end='')																	#print sent bit
+	#if b == 1:																			#if bit is 1, LED on
+		#GPIO.output(23, GPIO.HIGH)														#signal high
+	time.sleep(bit_time)																#wait bit_time for receiver to sample
+	#GPIO.output(23, GPIO.LOW)															#signal low after bit_time delay
 	return
 	
 
 def send_char(c):
-    print(c)
-    bit_string = bin(ord(c))[2:].zfill(7)
-    #print(bit_string)
-    for x in range (0, len(bit_string)):
-        send_bit(bit_string[x])
+    print(c + ": " , end='')															#print sent character
+    bit_string = bin(ord(c))[2:].zfill(7)												#format bit_string to remove 0b and force length 7
+    for x in range (0, len(bit_string)):												#loop through bit_string and transmit each bit
+        send_bit(bit_string[x])															#send each bit
+    print("")																			#new line after completed bit_string/character
     return
 
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(23,GPIO.OUT)
- 
+
+def main():
+	#GPIO.setmode(GPIO.BCM)
+	#GPIO.setup(23,GPIO.OUT)
+	x = 0
+	while True:
+	    current_time  = ((time.time()*1000)%1000)/1000.0								#query time and convert to seconds in range [0, 1.0)
+	    if (sender_id*0.5) <= current_time and current_time < (sender_id*0.5 + 0.5) : 	#check for valid time slice
+	        if sender_id*0.5+0.5 - current_time > 8 * bit_time : 						#check if enough time to send single character
+	            send_char(message[x])													#send single character
+	            x = (x+1)%len(message)													#increment message index and mod to loop back
 
 
-while True:
-    for x in range (0, len(message)):
-        current_time  = ((time.time()*1000)%1000)/1000.0
-        if (sender_id*0.5) <= current_time and current_time < (sender_id*0.5 + 0.5) :
-            #print("in window")
-            if sender_id*0.5+0.5 - current_time > 8 * bit_time :
-                #print("time")
-                send_char(message[x])
-        else :
-            print(current_time)
-            time.sleep(bit_time*10)
-        #print("no time")
-        			
+main() 
 
-
-
-
-
-
-while True:
-	GPIO.output(23, GPIO.HIGH)
-	time.sleep(1)
-	GPIO.output(23, GPIO.LOW)
-	time.sleep(1)
-
-
-
-while True:
-    for x in range (0, len(message)):
-        #print(message[x])
-        send_char(message[x])
-        time.sleep(0.1)
